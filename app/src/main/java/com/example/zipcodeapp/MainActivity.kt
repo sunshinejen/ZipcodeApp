d84611d561127d457c6e3134b851fc0ec30c80f4
Jennvillacis <jennvillacis@gmail.com>
2020-05-13 17:02:17 -0700

    Additions to weather app
    Assignment 4 additions

--- app/src/main/java/com/example/zipcodeapp/MainActivity.kt
+++ app/src/main/java/com/example/zipcodeapp/MainActivity.kt
@@ -1,23 +1,33 @@
 package com.example.zipcodeapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.zipcodeapp.details.ForecastDetailsActivity
 
 class MainActivity : AppCompatActivity() {
 
 
-    private val forecastRespository = ForecastRespository()
+    private val forecastRepository = ForecastRespository()
+    private lateinit var tempDisplaySettingManager: TempDisplaySettingManager
 
     //region setup methods
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tempDisplaySettingManager = TempDisplaySettingManager(this)
 
         val zipcodeEditText: EditText = findViewById(R.id.zipcodeEditText)
         val enterButton : Button = findViewById(R.id.enterButton)
@@ -30,7 +40,7 @@ class MainActivity : AppCompatActivity() {
                 //string.xml message
                 Toast.makeText(this, R.string.zipcode_entry_error, Toast.LENGTH_SHORT).show()
             } else{
-                forecastRespository.loadForecast(zipcode)
+                forecastRepository.loadForecast(zipcode)
             }
         }
 
@@ -39,11 +49,10 @@ class MainActivity : AppCompatActivity() {
         forecastList.layoutManager = LinearLayoutManager(this)
 
         //trailing lambda syntax, if you are passing a function to another function, if the function you are passing is the last argument then you can pass it outside the parenthesis DailyForecastAdapter(){}
-        val dailyForecastAdapter = DailyForecastAdapter(){forecastItem ->
+        val dailyForecastAdapter = DailyForecastAdapter(tempDisplaySettingManager){forecast ->
             //it -> implicit receiver type or you can rename like we did to forecastItem
+            showForecastDetails(forecast)
 
-            val msg = getString(R.string.forecast_clicked_format, forecastItem.temp, forecastItem.description)
-            Toast.makeText(this,msg, Toast.LENGTH_SHORT).show()
         }
         forecastList.adapter = dailyForecastAdapter
 
@@ -52,8 +61,37 @@ class MainActivity : AppCompatActivity() {
             //update our list adapter
             dailyForecastAdapter.submitList(forecastItems)
         }
-        forecastRespository.weeklyForecast.observe(this, weeklyForecastObserver)
+        forecastRepository.weeklyForecast.observe(this, weeklyForecastObserver)
 
 
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater : MenuInflater = menuInflater
+        inflater.inflate(R.menu.settings_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        //handle item selection
+        return when (item.itemId){
+            R.id.tempDisplaySetting -> {
+                //do something
+                showTempDisplaySettingDialog(this, tempDisplaySettingManager)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
+
+
+        private fun showForecastDetails(forecast: DailyForecast) {
+        val forecastDetailsIntent = Intent(this, ForecastDetailsActivity::class.java)
+        forecastDetailsIntent.putExtra("key_temp", forecast.temp)
+        forecastDetailsIntent.putExtra("key_description", forecast.description)
+        startActivity(forecastDetailsIntent)
     }
 }
